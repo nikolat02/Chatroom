98c667dead9502f2cfabaf5322c2573e0ea5ef53
ntirnanich <ntirnanich@yahoo.com>
2016-11-22 13:45:03 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatRoomClient
+{
+    class Program   //NameSpace ConsoleApplication1 is the ChatRoomClient side of the project
+    {
+        private static Stream stm;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new ClientForm());
+            try
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter username");
+                User clientUser;
+                clientUser = new User(Console.ReadLine());
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("192.168.101.9", 8001); //192.168.101.9
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+
+                stm = tcpclnt.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(clientUser.UserName);
+                stm.Write(ba, 0, ba.Length);
+
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = Console.ReadLine();
+                Thread replyThread = new Thread(ReceiveReplies);
+                replyThread.Start();
+                while (str != "Exit")
+                {
+
+                    ba = asen.GetBytes(str);
+                    Console.WriteLine("Transmitting.....");
+
+                    stm.Write(ba, 0, ba.Length); //This writes "the wire"
+                    str = Console.ReadLine();
+                }
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+
+        private static void ReceiveReplies()
+        {
+            byte[] bb = new byte[100];
+            while(true)
+            { 
+            int k = stm.Read(bb, 0, 100);
+
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(bb[i]));
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1    //NameSpace ConsoleApplication1 is the ChatRoomClient side of the project
+{
+    public class User
+    {
+        private string ipAddress;
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            private set
+            {
+                userName = value;
+            }
+        }
+
+        public string IPAddress
+        {
+            get { return ipAddress; }
+            private set
+            {
+                ipAddress = value;
+            }
+        }
+
+
+        public User(string user)
+        {
+            UserName = user;
+        }
+    }
+
+}
